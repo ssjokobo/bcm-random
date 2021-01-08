2df60030f12eeece39b420a733bd8835883e4b0f
ssjokobo <sopon.suwannakit@gmail.com>
2021-01-08 14:59:30 -0800

    change random multipler to length

--- bcm-random.js
+++ bcm-random.js
@@ -2,7 +2,7 @@ const pitch = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
 const chord = ['maj9', 'maj9#11', 'm11', 'm13', '13', '13b9', '9#11', '7#9b13', '13sus4', '7sus4b9', 'dim7', 'm7b5'];
 
 const random = (type) => {
-    let num = Math.floor(Math.random()*12);
+    let num = Math.floor(Math.random() * type.length);
     return type[num]    
 };
 
