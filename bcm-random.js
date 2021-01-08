5586c85e6724fd423b1be7158b505321922015aa
ssjokobo <sopon.suwannakit@gmail.com>
2021-01-08 14:39:08 -0800

    reduce to one function

--- bcm-random.js
+++ bcm-random.js
@@ -1,15 +1,9 @@
 const pitch = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
-const quality = ['maj9', 'maj9#11', 'm11', 'm13', '13', '13b9', '9#11', '7#9b13', '13sus4', '7sus4b9', 'dim7', 'm7b5'];
-const bass = () => {
+const chord = ['maj9', 'maj9#11', 'm11', 'm13', '13', '13b9', '9#11', '7#9b13', '13sus4', '7sus4b9', 'dim7', 'm7b5'];
+
+const random = (type) => {
     let num = Math.floor(Math.random()*12);
-    return pitch[num]
+    return type[num]    
 };
-const chord = () => {
-    let num = Math.floor(Math.random()*12);
-    return quality[num]
-};
-const melody = () => {
-    let num = Math.floor(Math.random()*12);
-    return pitch[num]
-};
-console.log('Use the melody pitch ' + melody() + ' with the chord ' + bass() + chord());
+
+console.log('Use the melody pitch ' + random(pitch) + ' with the chord ' + random(pitch) + random(chord));
